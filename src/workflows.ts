ae297086b910c99b65d526806c816936152e74af
Brian Hogan <brianhogan@napcs.com>
2024-12-16 09:51:54 -0600

    comments

--- src/workflows.ts
+++ src/workflows.ts
@@ -7,12 +7,12 @@ import type * as activities from './activities';
 // Load Activities and assign the Retry Policy
 const { getIP, getLocationInfo} = workflow.proxyActivities<typeof activities>({
   retry: {
-    initialInterval: '1 second',
-    maximumInterval: '1 minute',
-    backoffCoefficient: 2,
-    // maximumAttempts: 5,
+    initialInterval: '1 second', // amount of time that must elapse before the first retry occurs.
+    maximumInterval: '1 minute', // maximum interval between retries.
+    backoffCoefficient: 2, // how much the retry interval increases.
+    // maximumAttempts: 5, // maximum number of execution attempts. Unspecified means unlimited retries.
   },
-  startToCloseTimeout: '1 minute',
+  startToCloseTimeout: '1 minute', // maximum time allowed for a single Activity Task Execution.
 });
 
 // The Temporal Workflow.
